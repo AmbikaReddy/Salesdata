21e9f823fd74225808bd9fd388afbce394801c1e
trainee <trainee@sudaksha-030915>
2017-05-09 14:50:38 +0530

    First modified r

---
+++ salesTool/src/salesTool/Main.java
@@ -0,0 +1,17 @@
+package salesTool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		SalesData data =new SalesData();
+		displayGreetings();
+		data.display();
+	}
+	public static void displayGreetings(){
+		System.out.println("welcome git");
+	}
+
+}
---
+++ salesTool/src/salesTool/SalesData.java
@@ -0,0 +1,10 @@
+package salesTool;
+
+public class SalesData {
+	int[] data={0,4,12,2};
+	public void display()
+	{for(int i:data)
+		System.out.println( "Todo:"+data);
+	}
+
+}
